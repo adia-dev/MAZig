6d2cd5d67fe93ff48d3cfa5c7838677060336b99
Abdoulaye DIA <abdoudu78130@gmail.com>
2023-08-13 21:59:23 +0200

    Opening window

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,92 @@
+const c = @cImport({
+    @cInclude("SDL2/SDL.h");
+    @cInclude("SDL2/SDL_ttf.h");
+});
+
 const std = @import("std");
+const assert = @import("std").debug.assert;
+
+pub fn lerp(a: f32, b: f32, t: f32) f32 {
+    return a + (b - a) * t;
+}
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    if (c.SDL_Init(c.SDL_INIT_VIDEO) != 0) {
+        c.SDL_Log("Unable to initialize SDL: %s", c.SDL_GetError());
+        return error.SDLInitializationFailed;
+    }
+    defer c.SDL_Quit();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    if (c.TTF_Init() != 0) {
+        c.SDL_Log("Unable to initialize SDL_ttf: %s", c.SDL_GetError());
+        return error.SDLInitializationFailed;
+    }
+    defer c.TTF_Quit();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    const screen = c.SDL_CreateWindow("Mazes", c.SDL_WINDOWPOS_UNDEFINED, c.SDL_WINDOWPOS_UNDEFINED, 720, 480, c.SDL_WINDOW_OPENGL | c.SDL_RENDERER_PRESENTVSYNC) orelse {
+        c.SDL_Log("Unable to create window: %s", c.SDL_GetError());
+        return error.SDLInitializationFailed;
+    };
+    defer c.SDL_DestroyWindow(screen);
 
-    try bw.flush(); // don't forget to flush!
+    const renderer = c.SDL_CreateRenderer(screen, -1, 0) orelse {
+        c.SDL_Log("Unable to create renderer: %s", c.SDL_GetError());
+        return error.SDLInitializationFailed;
+    };
+    defer c.SDL_DestroyRenderer(renderer);
+
+    var quit = false;
+    var is_running = false;
+    // var last_time = c.SDL_GetTicks();
+
+    while (!quit) {
+        var event: c.SDL_Event = undefined;
+        while (c.SDL_PollEvent(&event) != 0) {
+            switch (event.type) {
+                c.SDL_QUIT => {
+                    quit = true;
+                },
+                c.SDL_KEYDOWN => {
+                    const key_code = event.key.keysym.sym;
+                    switch (key_code) {
+                        c.SDLK_ESCAPE => {
+                            quit = true;
+                        },
+                        c.SDLK_LSHIFT => {
+                            is_running = true;
+                        },
+                        else => {
+                            std.debug.print("Key: {} PRESSED.\n", .{key_code});
+                        },
+                    }
+                },
+                c.SDL_KEYUP => {
+                    const key_code = event.key.keysym.sym;
+                    if (key_code == c.SDLK_LSHIFT) {
+                        is_running = false;
+                    }
+                },
+                else => {},
+            }
+        }
+
+        // const current_time = c.SDL_GetTicks();
+        // const delta_time = current_time - last_time;
+        // last_time = current_time;
+        // const dt_seconds = @as(f32, @floatFromInt(delta_time)) / 1000.0;
+
+        _ = c.SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        _ = c.SDL_RenderClear(renderer);
+
+        c.SDL_RenderPresent(renderer);
+
+        c.SDL_Delay(17);
+    }
 }
 
 test "simple test" {
     var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
+    defer list.deinit();
     try list.append(42);
     try std.testing.expectEqual(@as(i32, 42), list.pop());
 }
